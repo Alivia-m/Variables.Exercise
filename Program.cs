286d983320acf7130a2ad7a45b3e4b09db9e295f
Alivia-m <aliviagayle@icloud.com>
2021-06-11 08:22:27 -0500

    variables exercise

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Variable.Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int catAge;
+            string catName;
+            bool isYellow;
+
+            catAge = 5;
+            catName = "lion";
+            isYellow = true;
+
+            Console.WriteLine($"The cats name is {catName}, he is {catAge} years old. Is he yellow? {isYellow}");
+
+            decimal a;
+            double b;
+            char c;
+
+            a = 7.9m;
+            b = +10.5;
+            c = '*';
+            Console.WriteLine(7.9 * +10.5);
+           
+            
+           
+        }
+    }
+}
